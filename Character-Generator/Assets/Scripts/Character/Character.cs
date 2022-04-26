1d1b8fb250a0ae2a4c95d25c4f8b67df9e3d9dca
Morph <chartouni.mathis@gmail.com>
2022-04-26 05:36:18 +0200

    Character core v2
    - Split the generation of new characters into two classes : the first class called BodyPart aims at creating the base body part while the second class, called CharacterBodyPart, inherits from the first one and is used in the inspector.

--- Character-Generator/Assets/Scripts/Character/Character.cs
+++ Character-Generator/Assets/Scripts/Character/Character.cs
@@ -6,15 +6,41 @@ using System.Collections.Generic;
 public class Character {
 
     public string name;
-    public List<BodyPart> bodyParts = new List<BodyPart>();
+    public List<CharacterBodyPart> bodyParts = new List<CharacterBodyPart>();
 
 }
 
 [System.Serializable]
 public class BodyPart {
 
+    public enum part {
+
+        Head,
+        Torso,
+
+    };
+
+    [Space(6)]
+    public part partType;
+
+    [Space(6)]
+    public string customName;
+
+    [Space(6)]
+    public GameObject prefab;
+
+}
+
+[System.Serializable]
+public class CharacterBodyPart : BodyPart {
+
+    [Space(6)]
     public string name;
+
+    [Space(6)]
     public Vector3 position;
+
+    [Space(6)]
     public bool required;
 
 }
