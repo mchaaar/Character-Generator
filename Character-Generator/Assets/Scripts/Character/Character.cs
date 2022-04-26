f198c28909e7df3928aecce19cc3a0c8591db2f6
Morph <chartouni.mathis@gmail.com>
2022-04-26 05:57:38 +0200

    Character core v3
    - Continued to split the body part classes.
    - Removed a duplicated string variable.
    - The character and the body part scripts will now be stored in separate folders.

--- Character-Generator/Assets/Scripts/Character/Character.cs
+++ Character-Generator/Assets/Scripts/Character/Character.cs
@@ -9,38 +9,3 @@ public class Character {
     public List<CharacterBodyPart> bodyParts = new List<CharacterBodyPart>();
 
 }
-
-[System.Serializable]
-public class BodyPart {
-
-    public enum part {
-
-        Head,
-        Torso,
-
-    };
-
-    [Space(6)]
-    public part partType;
-
-    [Space(6)]
-    public string customName;
-
-    [Space(6)]
-    public GameObject prefab;
-
-}
-
-[System.Serializable]
-public class CharacterBodyPart : BodyPart {
-
-    [Space(6)]
-    public string name;
-
-    [Space(6)]
-    public Vector3 position;
-
-    [Space(6)]
-    public bool required;
-
-}
